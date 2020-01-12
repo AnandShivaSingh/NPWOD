9a8110bede0de2ec36c57ea74f2ee8a83d1694db
Anand Singh <anandshivasinghnikumbh@outlook.com>
2020-01-12 14:08:43 +0000

    Adding all files

---
+++ NPWOD/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NPWOD.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
